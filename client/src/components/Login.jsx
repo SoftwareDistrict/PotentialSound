df28bb65d2e547b4444d5471f020ae69ceb43ef6
brandon <bsbrinoesos@gmail.com>
2020-09-01 09:35:50 -0500

    'made a simple splash page that has a google log in button'

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
 function Login(props) {
   
-    return <h1>{`Login Page`}</h1>;
-  }
+    return (
+      <div>
+        <h1>{`Welcome to Potential Sound`}</h1>
+        <div className='Googlelogin'>
+          <a href='/auth/google'><button className='GoogleButton'>Sign in with Google</button></a>
+        </div>
+      </div>
+
+
+      );
+    }
   export default Login

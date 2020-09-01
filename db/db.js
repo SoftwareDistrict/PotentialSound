2bc66a64a2f0561b7ecdd264218331452b2fc8f9
Evan Taft <evtaft22@gmail.com>
2020-09-01 16:29:03 -0500

    established postgreSQL connection

---
+++ db/db.js
@@ -0,0 +1,20 @@
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize('potentialsound', 'root', ' ', {
+  host: 'localhost',
+  dialect: 'postgres'
+});
+
+const connect = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+}
+connect();
+
+module.exports = {
+  sequelize
+}

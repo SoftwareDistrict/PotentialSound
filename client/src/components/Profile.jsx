a0923b5d4015c9538e9eb3de1678551b602a24e9
HenokSamuel <henoksamuel360@gmail.com>
2020-08-31 19:04:32 -0500

    Added dummy data for Profile info and setup Profile divs for description

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 function Profile(props) {
   
+    const [ state, setState ] = useState({
+        userName: 'Bob',
+        propic:'https://media.npr.org/assets/img/2020/02/06/marley-dennislawrence_wide-ff47e360977a27acfb066e56d6a98d3262619e27.jpeg?s=1400',
+        cell: '678-920-3121',
+        desc: 'All about love and music!',
+        city:  'Kingston',
+        email: "bob123@gmail.com"
+    });
     return (
         <div>
     <h1>{`Profile Page`}</h1>
     <div id='profile'>
-       
+    <h1>{`${state.userName}'s Profile Inforamtion`}</h1>
+               <div class='profileInfo'>{`Username: ${state.userName}`}</div>
+               <div class='profileInfo'>{`Email: ${state.email}`}</div>
+               <div class='profileInfo'>{`Cell Phone Number: ${state.cell}`}</div>
+               <div class='profileInfo'>{`Hometown: ${state.city}`}</div>
+               <div class='profileInfo'>{`Profile Pic:`}<img src={state.propic}/></div> 
         </div>
     </div>
     )

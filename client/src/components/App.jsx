cdff9d6e3b00d9941544d5262e4df30e06bc2227
HenokSamuel <henoksamuel360@gmail.com>
2020-09-01 15:58:48 -0500

    Added routes for componenets

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import Login from './Login.jsx'
 import HomeFeed from './HomeFeed.jsx'
 import Profile from './Profile.jsx'
+import CreatePostMessage from './CreatePostMessage.jsx'
+import PostEntry from './PostEntry.jsx'
+import PostTitleEntry from './PostTitleEntry.jsx'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 class App extends Component {
   constructor(props) {
     super(props)
@@ -9,12 +13,16 @@ class App extends Component {
   }
   render() {
     return (
+              <Router>
       <div>
-        <h1>TESTING</h1>
-        <Login />
-        <HomeFeed />
-        <Profile />
+        <Switch>
+               <Route exact={true} path="/" render={() => (<HomeFeed />)} />
+               <Route exact={true} path="/login" render={() => (<Login />)} />
+               <Route exact={true} path="/profile" render={() => (<Profile />)} />
+               <Route exact={true} path="/createPostMessage" render={() => (<CreatePostMessage />)} />
+            </Switch>
       </div>
+            </Router>
     )
   }
 }

587d17e9b5b6d86a85974065885df41ce469dca2
HenokSamuel <henoksamuel360@gmail.com>
2020-09-02 04:12:04 -0500

    Got rid of hard coded posts for feed and rendered PostFeedEntry componenet dynamiclly  and added a Link to createPostMessage component

--- client/src/components/HomeFeed.jsx
+++ client/src/components/HomeFeed.jsx
@@ -1,37 +1,35 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import PostFeedEntry from './PostFeedEntry.jsx'
 const HomeFeed = (props) => {
+	console.log(props);
 
-    return (
-        <div>
-            <h1>
+	const { arrPosts } = props
+	return (
+		<div>
+			<h1>
 				PotentialSound
 				<div style={{ width: "100px", height: "100px", borderRadius: '50%', position: 'relative', overflow: 'hidden' }}>
-					<img src="https://tinyurl.com/y3h5vk9r" alt="Avatar" style={{ width: "auto", height: "100%", display: 'inline', margin: '0 auto', marginLeft: '-25%' }}/>
+					<img src="https://tinyurl.com/y3h5vk9r" alt="Avatar" style={{ width: "auto", height: "100%", display: 'inline', margin: '0 auto', marginLeft: '-25%' }} />
 				</div>
 				<button type="button">MENU</button>
 			</h1>
 			<div>
-				<input type="text" placeholder="Search for Post" style={{  width: '500px', height: '30px', fontSize: '14px', paddinLeft: '10px' }}></input>
+				<input type="text" placeholder="Search for Post" style={{ width: '500px', height: '30px', fontSize: '14px', paddinLeft: '10px' }}></input>
 				<button type="button" style={{ borderRadius: '5px' }}>
-					<img src="https://tinyurl.com/y2v9h8rz" style={{ width: "15%", height: "15%" }}/>
+					<img src="https://tinyurl.com/y2v9h8rz" style={{ width: "15%", height: "15%" }} />
 				</button>
 				<div>
-					{/* <Link to="/"><button type="button" style={{ fontSize: '20px', padding: '5px', paddingLeft: '10px', paddingRight: '10px', borderRadius: '5px' }}>Creat A Post</button></Link> */}
-					<Link to="/createPostMessage">Creat A Post</button></Link>
+					<Link to="/createPostMessage"><button type="button" style={{ fontSize: '20px', padding: '5px', paddingLeft: '10px', paddingRight: '10px', borderRadius: '5px' }}>Creat A Post</button></Link>
+
 				</div>
 			</div>
-			<div style={{  backgroundColor: 'rgb(40, 84, 52)', height: '500px' }}>
-				<h1>General Feed</h1>
-				<h2>POST</h2>
-				<h2>POST</h2>
-				<h2>POST</h2>
-				<h2>POST</h2>
-				<h2>POST</h2>
-				<h2>POST</h2>
+			<div style={{ backgroundColor: "rgb(200,200,200)", height: '500px' }}>
+
+				{arrPosts.map(post => <div><PostFeedEntry post={post} /><br /></div>)}
 			</div>
-        </div>
-    );
+		</div>
+	);
 }
 
 export default HomeFeed

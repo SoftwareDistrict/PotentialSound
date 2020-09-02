259c8f911b76fab3aa459f3e4c3ef25437a4ac31
HenokSamuel <henoksamuel360@gmail.com>
2020-09-02 04:07:37 -0500

    Added PostFullMessage component

---
+++ client/src/components/PostFullMessage.jsx
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react';
+function PostFullMessage({ arrPosts, match }) {
+
+
+    const name = match.match.params.id;
+
+    var found = arrPosts.find(function (element) {
+        return name === element.posterName;
+    });
+    const { posterName, message, profilePic } = found
+  
+   
+  
+    const onEvent = (event, setFunc, val) => {
+        if (event.target.value === '' || event.target.value === undefined) {
+            setFunc(val);
+        } else {
+            setFunc(event.target.value);
+            console.log(message);
+        }
+    }
+    return (
+        <div>
+            <div id='profile' style={{border: '2px solid black', width: '500px', height: '300px', textAlign: 'center', margin: '0 auto', position: "relative"}}>
+            <div style={{fontSize: '125%'}}>{`${posterName} posted`}</div>
+               <div>{`Message: ${message}`}</div><br/>
+               <div style={{ position: "absolute", top: '5', textAlign: 'center', resize: 'both', overflow: 'auto', width: '150px', height: '150px', textAlign: 'center' }}>{`Profile Pic:`}<img style={{ maxWidth: "100%", maxHeight: "100%" }} src={profilePic}/></div><br/>
+        </div>
+        <div>
+            <h3>Reply</h3>
+            <label>Message  <input size="60" onChange={(event) => onEvent(event, setTitle, title)} type="text" placeholder={'Message'} /></label><br /><br />
+            <button>Submit</button>
+        </div>
+
+        </div>
+    )
+}
+export default PostFullMessage

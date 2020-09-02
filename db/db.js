84a426c1e5b2615214d3eac30b39f899ee2ca8c4
brandon <bsbrinoesos@gmail.com>
2020-09-01 19:29:11 -0500

    Users and Chats are being added to postgres

--- db/db.js
+++ db/db.js
@@ -14,6 +14,97 @@ const connect = async () => {
   }
 }
 connect();
+// creating the tables
+const User = sequelize.define('User', {
+  userName: Sequelize.STRING,
+  city: Sequelize.STRING,
+  desciption: Sequelize.STRING,
+  googleId: Sequelize.STRING,
+  gmail: Sequelize.STRING,
+  cell: Sequelize.TEXT,
+});
+
+// const Messages = sequelize.define('Messages', {
+//   message: Sequelize.STRING,
+//   userId: {
+//     type: Sequelize.INTEGER,
+//     references: {
+//       model: 'Users',
+//       referencesKey: 'id'
+//   }
+// },
+//   chatId: {
+//     type: Sequelize.INTEGER,
+//     references: {
+//       model: 'Chats',
+//       referencesKey: 'id'
+//     }
+//   },
+  
+//   created_at: {
+//     type: 'TIMESTAMP',
+//     defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+//     allowNull: false
+//   },
+//   updated_at: {
+//     type: 'TIMESTAMP',
+//     defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+//     allowNull: false
+//   }
+// })
+
+const Chats = sequelize.define('Chats', {
+  name: Sequelize.STRING,
+})
+
+// const ChatJoin = sequelize.define('ChatJoin', {
+//   userId: {
+//     type: Sequelize.INTEGER,
+//     references: {
+//       model: 'Users',
+//       referencesKey: 'id'
+//     }
+//   },
+//   chatId: {
+//     type: Sequelize.INTEGER,
+//     references: {
+//       model: 'Chats',
+//       referencesKey: 'id'
+//     }
+//   }
+// })
+
+// const Posts = sequelize.define('Post', {
+//   userId: {
+//     type: Sequelize.INTEGER,
+//     references: {
+//       model: 'Users',
+//       referencesKey: 'id'
+//   },
+//   message: Sequelize.STRING,
+//   created_at: {
+//     type: 'TIMESTAMP',
+//     defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+//     allowNull: false
+//   },
+//   updated_at: {
+//     type: 'TIMESTAMP',
+//     defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+//     allowNull: false
+//    }
+//   }
+// })
+
+// const Tags = sequelize.define('Tags', {
+//   postId: {
+//     type: Sequelize.INTEGER,
+//     references: {
+//       model: 'Posts',
+//       referencesKey: 'id'
+//     }
+//   },
+//   tag: Sequelize.STRING
+// })
 
 
 // .then(() => sequelize.query('createdb potentialsound -O root'))
@@ -23,18 +114,14 @@ connect();
 // .then(() => sequelize.query('GRANT ALL PRIVILEGES ON DATABASE potentialsound to root'))
 // .then(() => sequelize.query('\c potentialsound'))
 // .then(() => {
-  const User = sequelize.define('User', {
-    userName: Sequelize.STRING,
-    city: Sequelize.STRING,
-    desciption: Sequelize.STRING,
-    googleId: Sequelize.STRING,
-    gmail: Sequelize.STRING,
-    cell: Sequelize.TEXT,
-  });
 //   })
 
-User.sync()
-
+User.sync({ force: true});
+// Messages.sync({ force: true});
+Chats.sync({ force: true});
+// ChatJoin.sync({ force: true});
+// Posts.sync({ force: true});
+// Tags.sync({ force: true});
 
 
 module.exports = {

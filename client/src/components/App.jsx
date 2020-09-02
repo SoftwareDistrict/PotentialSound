c5537c408c42596827a472d512e8a6aa3b6f4649
HenokSamuel <henoksamuel360@gmail.com>
2020-09-02 10:02:13 -0500

    Added tags to dummy data and created function to add a new post

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -15,21 +15,32 @@ class App extends Component {
         posterName: 'Kenny',
         title: 'Looking for a guitarist for a band!',
         message:'My band and I just lost our drummer who moved out the country. We are a local indie band in New Orleans and looking for someone who loves making bad ass music!',
-        profilePic: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ0XfgQMRh9MHNpd76OMN6r-SmwoE9HA_x92Q&usqp=CAU'}, 
+        profilePic: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ0XfgQMRh9MHNpd76OMN6r-SmwoE9HA_x92Q&usqp=CAU',
+        tags: ['#help-wanted', '#rock']
+      }, 
         {
         posterName: 'LitLil',
         title: 'Looking for producer that makes hiphop beats!',
         message:'I got some bars who got the heat!',
         profilePic: 'https://i.pinimg.com/originals/ce/f5/c2/cef5c2b4199157a05d50ec79fd8dc539.jpg',
+        tags: ['#help-wanted'],
       },
       {
         posterName: 'Sara',
         message:'Looking for a lead vocalist to join our choir!',
         profilePic: 'https://www.saintmarys.edu/files/images/Womens-Choir-WEB-2.jpg',
+        tags: ['#help-wanted', '#vocalist'],
       }
     ]
     }
+    this.makeNewPost = this.makeNewPost.bind(this);
   }
+
+makeNewPost(post){
+  this.state.arrPosts.push(post);
+}
+
+
   render() {
     const { arrPosts } = this.state;
     return (
@@ -39,7 +50,7 @@ class App extends Component {
                <Route exact={true} path="/" render={() => (<HomeFeed arrPosts={arrPosts} />)} />
                <Route exact={true} path="/login" render={() => (<Login />)} />
                <Route exact={true} path="/profile/:id" render={() => (<Profile />)} />
-               <Route exact={true} path="/createPostMessage" render={() => (<CreatePostMessage />)} />
+               <Route exact={true} path="/createPostMessage" render={() => (<CreatePostMessage makeNewPost={this.makeNewPost} />)} />
                <Route path="/fullMessage/:id" render={(match) => (<PostFullMessage arrPosts={arrPosts} match={match}  />)} />
                {/* <Route path="/fullMessage/:id" component={() => (<PostFullMessage arrPosts={arrPosts}  />)} /> */}
             </Switch>

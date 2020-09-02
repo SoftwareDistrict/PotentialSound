06db92de09140a3c7f396c70af4b8beeac429a38
HenokSamuel <henoksamuel360@gmail.com>
2020-09-02 04:03:03 -0500

    PostTitleEntry renamed to PostFeedEntry &  completed basic functionailty for the component

---
+++ client/src/components/PostFeedEntry.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+function PostFeedEntry(props) {
+    const { posterName, message, profilePic } = props.post
+    const tags = ['r&b', 'rap', 'soul']
+    return (
+
+        <div>
+            <Link style={{ color: '#ff8c00' }} to={`/fullMessage/${posterName}`}>
+                <div id='profile' style={{ border: '2px solid black', width: '400px', height: '100px', margin: '0 auto', backgroundColor: '#3F3D3D', position: "relative" }}>
+                    <div style={{ position: "absolute", top: '0', resize: 'both', overflow: 'auto', width: '300px', height: '20px', textAlign: 'center', left: '100px', fontSize: '14px' }}>{`${posterName}`}</div>
+                    <div style={{ position: "absolute", top: '5', resize: 'both', overflow: 'auto', width: '100px', height: '100px', textAlign: 'center' }}><img style={{ maxWidth: "100%", maxHeight: "100%" }} src={profilePic} /></div>
+                    <div style={{ position: "absolute", top: '20px', backgroundColor: '#ffedd8', resize: 'both', overflow: 'auto', width: '300px', height: '60px', textAlign: 'center', left: '100px', fontSize: '12px' }}>{message}</div>
+                    <div style={{ position: "absolute", bottom: '0', resize: 'both', overflow: 'auto', width: '300px', height: '20px', textAlign: 'center', left: '100px' }}><div>{tags.join('  ')}</div></div>
+                </div>
+            </Link>
+        </div>
+
+    )
+}
+export default PostFeedEntry

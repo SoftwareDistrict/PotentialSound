ac58555db1cf3502bd8dd47e30e250195ca7fd07
DennisPintilie <dennis.pintilie.alexandru@gmail.com>
2020-09-02 16:23:23 -0500

    User can update their username, city, cell and/or description

--- client/src/components/UpdateProfile.jsx
+++ client/src/components/UpdateProfile.jsx
@@ -1,8 +1,23 @@
 import React, {useState} from 'react';
 
 const UpdateProfile = () => {
+
+    const [newUser, setNewUser] = useState('Previous Username');
+    const [newCity, setNewCity] = useState('Previous City');
+    const [newCell, setNewCell] = useState('Previous Cell');
+    const [newDescription, setNewDescription] = useState('Previous Description');
+
+    const updateProfile = () => {
+        console.log({newUser, newCity, newCell, newDescription}, 'hit');
+    }
+
     return(<div>
-        <h3>Update user here</h3>
+        <h1>Update Your Profile</h1>
+        <input value={newUser} onChange={(e) => {setNewUser(e.target.value)}}></input><br/>
+        <input value={newCity} onChange={(e) => {setNewCity(e.target.value)}}></input><br/>
+        <input value={newCell} onChange={(e) => {setNewCell(e.target.value)}}></input><br/>
+        <input value={newDescription} onChange={(e) => {setNewDescription(e.target.value)}}></input><br/>
+        <button onClick={() => updateProfile()}>Submit Change</button>
     </div>)
 }
 

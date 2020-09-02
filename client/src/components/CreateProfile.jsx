0f764ed1734f1307482ccfb768014826c4b9348d
DennisPintilie <dennis.pintilie.alexandru@gmail.com>
2020-09-02 12:28:15 -0500

    Created CreateProfile component

---
+++ client/src/components/CreateProfile.jsx
@@ -0,0 +1,11 @@
+import React, {useState, useEffect} from 'react';
+
+function CreateProfile(){
+    return(
+        <div>
+            <h1>Create Profile</h1>
+        </div>
+    )
+}
+
+export default CreateProfile;

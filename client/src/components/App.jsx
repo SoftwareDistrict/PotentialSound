faac6034374a069fc3f47248bc926c10560e00ae
HenokSamuel <henoksamuel360@gmail.com>
2020-09-02 04:05:43 -0500

    Added hardcorded data for state to app comp and refactored routes for createpost, fullMessage, profile components

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -3,23 +3,45 @@ import Login from './Login.jsx'
 import HomeFeed from './HomeFeed.jsx'
 import Profile from './Profile.jsx'
 import CreatePostMessage from './CreatePostMessage.jsx'
-import PostEntry from './PostEntry.jsx'
-import PostTitleEntry from './PostTitleEntry.jsx'
+import PostFullMessage from './PostFullMessage.jsx'
+import PostFeedEntry from './PostFeedEntry.jsx'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 class App extends Component {
   constructor(props) {
     super(props)
-    this.state = {}
+    this.state = {
+      arrPosts: [{
+        posterName: 'Kenny',
+        title: 'Looking for a guitarist for a band!',
+        message:'My band and I just lost our drummer who moved out the country. We are a local indie band in New Orleans and looking for someone who loves making bad ass music!',
+        profilePic: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ0XfgQMRh9MHNpd76OMN6r-SmwoE9HA_x92Q&usqp=CAU'}, 
+        {
+        posterName: 'LitLil',
+        title: 'Looking for producer that makes hiphop beats!',
+        message:'I got some bars who got the heat!',
+        profilePic: 'https://i.pinimg.com/originals/ce/f5/c2/cef5c2b4199157a05d50ec79fd8dc539.jpg',
+      },
+      {
+        posterName: 'Sara',
+        message:'Looking for a lead vocalist to join our choir!',
+        profilePic: 'https://www.saintmarys.edu/files/images/Womens-Choir-WEB-2.jpg',
+      }
+    ]
+    }
   }
   render() {
+    const { arrPosts } = this.state;
     return (
               <Router>
       <div>
         <Switch>
-               <Route exact={true} path="/" render={() => (<HomeFeed />)} />
+               <Route exact={true} path="/" render={() => (<HomeFeed arrPosts={arrPosts} />)} />
                <Route exact={true} path="/login" render={() => (<Login />)} />
-               <Route exact={true} path="/profile" render={() => (<Profile />)} />
+               <Route exact={true} path="/profile/:id" render={() => (<Profile />)} />
                <Route exact={true} path="/createPostMessage" render={() => (<CreatePostMessage />)} />
+               <Route path="/fullMessage/:id" render={(match) => (<PostFullMessage arrPosts={arrPosts} match={match}  />)} />
+               {/* <Route path="/fullMessage/:id" component={() => (<PostFullMessage arrPosts={arrPosts}  />)} /> */}
             </Switch>
       </div>
             </Router>

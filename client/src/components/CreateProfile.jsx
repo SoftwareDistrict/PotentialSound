41198affa1d239a2e7f1d6686c5a7a8216a97687
DennisPintilie <dennis.pintilie.alexandru@gmail.com>
2020-09-02 16:48:20 -0500

    deleted console log comment

--- client/src/components/CreateProfile.jsx
+++ client/src/components/CreateProfile.jsx
@@ -9,7 +9,6 @@ const CreateProfile = () => {
     const [description, setDescription] = useState('');
 
     const createProfile = () => {
-        // console.log({username, city, cell, description});
         axios.post('/createProfile', {
             userName: username,
             city: city,

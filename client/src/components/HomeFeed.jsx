d06091b76810269a51196b417438ef5896d8375d
DennisPintilie <dennis.pintilie.alexandru@gmail.com>
2020-09-03 13:57:49 -0500

    Updated eslint and addressed linting errors

--- client/src/components/HomeFeed.jsx
+++ client/src/components/HomeFeed.jsx
@@ -1,35 +1,89 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import PostFeedEntry from './PostFeedEntry.jsx'
-const HomeFeed = (props) => {
-	console.log(props);
+import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import PostFeedEntry from "./PostFeedEntry.jsx";
 
-	const { arrPosts, userName } = props
-	return (
-		<div>
-			<Link to="/createProfile">CreateProfile</Link><br/>
-			<Link to={`/profile/${userName}`}> Go to Profile Page </Link>
-			<h1>
-				PotentialSound
-				<div style={{ width: "100px", height: "100px", borderRadius: '50%', position: 'relative', overflow: 'hidden' }}>
-					<img src="https://tinyurl.com/y3h5vk9r" alt="Avatar" style={{ width: "auto", height: "100%", display: 'inline', margin: '0 auto', marginLeft: '-25%' }} />
-				</div>
-				<button type="button">MENU</button>
-			</h1>
-			<div>
-				<input type="text" placeholder="Search for Post" style={{ width: '500px', height: '30px', fontSize: '14px', paddinLeft: '10px' }}></input>
-				<button type="button" style={{ borderRadius: '5px' }}>
-					<img src="https://tinyurl.com/y2v9h8rz" style={{ width: "15%", height: "15%" }} />
-				</button>
-				<div>
-					<Link to="/createPostMessage"><button type="button" style={{ fontSize: '20px', padding: '5px', paddingLeft: '10px', paddingRight: '10px', borderRadius: '5px' }}>Creat A Post</button></Link>
-				</div>
-			</div>
-			<div style={{ backgroundColor: "rgb(200,200,200)", height: '500px' }}>
-				{arrPosts.map(post => <div><PostFeedEntry post={post} /><br /></div>)}
-			</div>
-		</div>
-	);
-}
+const HomeFeed = ({ arrPosts, userName }) => {
 
-export default HomeFeed
+  const posts = arrPosts;
+
+  return (
+    <div>
+      <Link to="/createProfile">CreateProfile</Link>
+      <br />
+      <Link to={`/profile/${userName}`}> Go to Profile Page </Link>
+      <h1>
+        PotentialSound
+        <div
+          style={{
+            width: "100px",
+            height: "100px",
+            borderRadius: "50%",
+            position: "relative",
+            overflow: "hidden",
+          }}
+        >
+          <img
+            src="https://tinyurl.com/y3h5vk9r"
+            alt="Avatar"
+            style={{
+              width: "auto",
+              height: "100%",
+              display: "inline",
+              margin: "0 auto",
+              marginLeft: "-25%",
+            }}
+          />
+        </div>
+        <button type="button">MENU</button>
+      </h1>
+      <div>
+        <input
+          type="text"
+          placeholder="Search for Post"
+          style={{ width: "500px", height: "30px", fontSize: "14px", paddinLeft: "10px" }}
+        ></input>
+        <button type="button" style={{ borderRadius: "5px" }}>
+          <img src="https://tinyurl.com/y2v9h8rz" style={{ width: "15%", height: "15%" }} />
+        </button>
+        <div>
+          <Link to="/createPostMessage">
+            <button
+              type="button"
+              style={{
+                fontSize: "20px",
+                padding: "5px",
+                paddingLeft: "10px",
+                paddingRight: "10px",
+                borderRadius: "5px",
+              }}
+            >
+              Create A Post
+            </button>
+          </Link>
+        </div>
+      </div>
+      <div style={{ backgroundColor: "rgb(200,200,200)", height: "500px" }}>
+        {posts.map((post, i) => (
+          <div key={`${i}`}>
+            <PostFeedEntry post={post} />
+            <br />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+HomeFeed.propTypes = {
+  arrPosts: PropTypes.arrayOf(PropTypes.shape({
+    posterName: PropTypes.string,
+    title: PropTypes.string,
+    message: PropTypes.string,
+    profilePic: PropTypes.string,
+    tags: PropTypes.arrayOf(PropTypes.string)
+  })),
+  userName: PropTypes.string.isRequired
+};
+
+export default HomeFeed;
--- server/index.js
+++ server/index.js
@@ -1,26 +1,26 @@
-require('dotenv').config();
-require('../db/db.js');
-const express = require('express');
-const path = require('path');
-const cors = require('cors');
+require("dotenv").config();
+require("../db/db.js");
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
 
 const PORT = process.env.PORT || 3000;
-const CLIENT_PATH = path.join(__dirname, '../client/dist');
+const CLIENT_PATH = path.join(__dirname, "../client/dist");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(express.static(CLIENT_PATH));
 
-app.post('/createProfile', (req, res) => {
+app.post("/createProfile", (req, res) => {
   const body = req.body;
   res.send(body).sendStatus(201);
-})
+});
 
-app.put('/updateProfile', (req, res) => {
+app.put("/updateProfile", (req, res) => {
   const body = req.body;
   res.send(body).sendStatus(201);
-})
+});
 
 app.listen(PORT, () => {
   console.info(`App listening at http://localhost:${PORT}`);

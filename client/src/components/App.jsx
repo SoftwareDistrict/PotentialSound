767192bede4d0ad3d69f645ce8d70cadaae8105e
Evan Taft <evtaft22@gmail.com>
2020-09-03 21:01:58 -0500

    refactor/ dummy data to fit actual syntax for db

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -7,50 +7,30 @@ import PostFullMessage from "./PostFullMessage.jsx";
 import CreateProfile from "./CreateProfile.jsx";
 import UpdateProfile from "./UpdateProfile.jsx";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import axios from "axios";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      arrPosts: [
-        {
-          posterName: "Kenny",
-          title: "Looking for a guitarist for a band!",
-          message:
-            "My band and I just lost our drummer who moved out the country. We are a local indie band in New Orleans and looking for someone who loves making bad ass music!",
-          profilePic:
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ0XfgQMRh9MHNpd76OMN6r-SmwoE9HA_x92Q&usqp=CAU",
-          tags: ["#help-wanted", "#rock"],
-        },
-        {
-          posterName: "LitLil",
-          title: "Looking for producer that makes hiphop beats!",
-          message: "I got some bars who got the heat!",
-          profilePic:
-            "https://i.pinimg.com/originals/ce/f5/c2/cef5c2b4199157a05d50ec79fd8dc539.jpg",
-          tags: ["#help-wanted"],
-        },
-        {
-          posterName: "Sara",
-          message: "Looking for a lead vocalist to join our choir!",
-          profilePic: "https://www.saintmarys.edu/files/images/Womens-Choir-WEB-2.jpg",
-          tags: ["#help-wanted", "#vocalist"],
-        },
-      ],
-      userName: "Bob",
-      userProfilePic:
-        "https://mma.prnewswire.com/media/1138309/UMe_Bob_Marley_1973_London.jpg?p=publish",
+      generalFeed: [],
     };
-
-    this.makeNewPost = this.makeNewPost.bind(this);
+    this.getAllPosts = this.getAllPosts.bind(this);
   }
 
-  makeNewPost(post) {
-    this.state.arrPosts.push(post);
+  componentDidMount() {
+    this.getAllPosts();
+  }
+
+  getAllPosts() {
+    axios
+      .get("/feed")
+      .then((feed) => this.setState({ geneeralFeed: feed.data }))
+      .catch((err) => console.warn("Could not get all posts", err));
   }
 
   render() {
-    const { arrPosts, userName, userProfilePic } = this.state;
+    const { generalFeed } = this.state;
     return (
       <div>
         <Router>
@@ -59,30 +39,16 @@ class App extends Component {
             <Route
               exact={true}
               path="/home"
-              render={() => <HomeFeed arrPosts={arrPosts} userName={userName} />}
+              render={() => <HomeFeed generalFeed={generalFeed} />}
             />
-            <Route
-              exact={true}
-              path="/profile/:id"
-              render={() => <Profile userName={userName} />}
-            />
-            <Route
-              exact={true}
-              path="/createPostMessage"
-              render={() => (
-                <CreatePostMessage
-                  makeNewPost={this.makeNewPost}
-                  userName={userName}
-                  userProfilePic={userProfilePic}
-                />
-              )}
-            />
+            <Route exact={true} path="/profile/:id" render={() => <Profile />} />
+            <Route exact={true} path="/createPostMessage" render={() => <CreatePostMessage />} />
             <Route
               path="/fullMessage/:id"
-              render={(match) => <PostFullMessage arrPosts={arrPosts} id={match.match.params.id} />}
+              render={(match) => <PostFullMessage id={match.match.params.id} />}
             />
             <Route path="/createProfile" render={() => <CreateProfile />} />
-            <Route path="/updateProfile" render={() => <UpdateProfile userName={userName} />} />
+            <Route path="/updateProfile" render={() => <UpdateProfile />} />
           </Switch>
         </Router>
       </div>

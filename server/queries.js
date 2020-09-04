5cab39ac71c314e1ebaebdef8a7f0aab20d056f1
Evan Taft <evtaft22@gmail.com>
2020-09-03 20:58:00 -0500

    (add) queries.js file to host all db queries

---
+++ server/queries.js
@@ -0,0 +1,15 @@
+const {
+  Users,
+  Posts,
+  Tags,
+} = require("./db");
+
+const getPosts = () => Posts.findAll();
+const getPoster = (id) => Users.findOne({ where: { id: id } });
+const getPostTags = (id) => Tags.findAll({ where: { id_post: id } });
+
+module.exports = {
+  getPosts,
+  getPoster,
+  getPostTags,
+};

e855a91d5a4805d09c4c4fa151a15a4f985cd4cc
Evan Taft <evtaft22@gmail.com>
2020-09-06 13:01:23 -0500

    (add)Google icon to sign in button

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -6,7 +6,30 @@ const Login = () => {
       <h1>Welcome to Potential Sound</h1>
       <div className="Googlelogin">
         <a href="/google">
-          <button className="GoogleButton">Sign in with Google</button>
+          <button
+            className="GoogleButton"
+            style={{
+              fontSize: "18px",
+              border: "none",
+              backgroundColor: "#037bfc",
+              width: "250px",
+              height: "60px",
+              textAlign: "right",
+              padding: "10px",
+            }}
+          >
+            <img
+              src="https://tinyurl.com/y6kf4vx4"
+              style={{
+                height: "50px",
+                width: "50px",
+                position: "absolute",
+                top: "120px",
+                left: "20px",
+              }}
+            />
+            Sign in with Google
+          </button>
         </a>
       </div>
     </div>

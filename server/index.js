fc88a60cd216240dcc7a44d4bda36ccabf5f3ce1
DennisPintilie <dennis.pintilie.alexandru@gmail.com>
2020-09-12 15:07:56 -0500

    (cleanup) Fix linting errors

--- server/index.js
+++ server/index.js
@@ -289,7 +289,7 @@ app.get("/getallchats", (req, res) => {
 app.post("/createLiveStream", (req, res) => {
   const obj = req.body;
   addLiveStream(obj)
-    .then((data) => res.status(200).json({"id": data.id}))
+    .then((data) => res.status(200).json({ id: data.id }))
     .catch((err) => res.status(500).send(err));
 });
 

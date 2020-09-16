0ae23f8dc9c40e4cb66de2da1571ad37c83ffb59
BrandonSB <bsbrionesos.gmail.com>
2020-09-16 08:14:47 -0500

    (add) color to social media icons

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Appbar from "./Appbar.jsx";
 import PropTypes from "prop-types";
-import { Avatar } from "@material-ui/core";
+import { Avatar, Grid } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import axios from "axios";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -54,70 +54,77 @@ const Profile = ({ currentUser }) => {
   return (
     <div>
       <Appbar currentUser={currentUser} />
-      <Avatar alt={username} src={propic} className={classes.avatar} />
-      <h1 style={{ textAlign: "right", marginTop: "80px" }}>{username}</h1>
-      {youTube ? (
-        <a href={youTube}>
-          <FontAwesomeIcon icon={faYoutube} />
-        </a>
-      ) : null}
-      {instagram ? (
-        <a href={instagram}>
-          <FontAwesomeIcon icon={faInstagram} />
-        </a>
-      ) : null}
-      {soundCloud ? (
-        <a href={soundCloud}>
-          <FontAwesomeIcon icon={faSoundcloud} />
-        </a>
-      ) : null}
-      {facebook ? (
-        <a href={facebook}>
-          <FontAwesomeIcon icon={faFacebook} />
-        </a>
-      ) : null}
-      <div
-        id="profile"
-        style={{
-          border: "2px solid black",
-          width: "350px",
-          height: "300px",
-          textAlign: "left",
-          fontSize: "20px",
-          margin: "0 auto",
-          color: "orange",
-        }}
+      <Grid
+        container
+        direction="row"
+        justify="center"
+        alignItems="center"
       >
-        <div style={{ marginBottom: "10px", marginTop: "25px" }} className="profileInfo">
-          Email: {email}
-        </div>
-        <div style={{ marginBottom: "10px" }} className="profileInfo">
-          Cell Phone Number: {cell}
-        </div>
-        <div style={{ marginBottom: "10px" }} className="profileInfo">
-          Based Out Of: {city}
-        </div>
-        <div style={{ marginBottom: "10px" }} className="profileInfo">
-          Description: {description}
-        </div>
-      </div>
-      <Link to="/updateProfile">
-        <button style={{ backgroundColor: "#eb8c34", marginTop: "10px" }} type="button">
-          Update Profile
-        </button>
-      </Link>
-      <div>
-        {InstaPosts[0] ? (
-          <div>
-            <img src={InstaPosts[0].imageUrl} />
-            <img src={InstaPosts[1].imageUrl} />
-            <img src={InstaPosts[2].imageUrl} />
-            <img src={InstaPosts[3].imageUrl} />
-            <img src={InstaPosts[4].imageUrl} />
-            <img src={InstaPosts[5].imageUrl} />
-          </div>
+        <Avatar alt={username} src={propic} className={classes.avatar} />
+        <h1 style={{ textAlign: "right", marginTop: "80px" }}>{username}</h1>
+        {youTube ? (
+          <a href={youTube}>
+            <FontAwesomeIcon color="red" icon={faYoutube} />
+          </a>
         ) : null}
-      </div>
+        {instagram ? (
+          <a href={instagram}>
+            <FontAwesomeIcon color="#e4405f" icon={faInstagram} />
+          </a>
+        ) : null}
+        {soundCloud ? (
+          <a href={soundCloud}>
+            <FontAwesomeIcon color="orange" icon={faSoundcloud} />
+          </a>
+        ) : null}
+        {facebook ? (
+          <a href={facebook}>
+            <FontAwesomeIcon color="blue" icon={faFacebook} />
+          </a>
+        ) : null}
+        <div
+          id="profile"
+          style={{
+            border: "2px solid black",
+            width: "350px",
+            height: "300px",
+            textAlign: "left",
+            fontSize: "20px",
+            margin: "0 auto",
+            color: "orange",
+          }}
+        >
+          <div style={{ marginBottom: "10px", marginTop: "25px" }} className="profileInfo">
+            Email: {email}
+          </div>
+          <div style={{ marginBottom: "10px" }} className="profileInfo">
+            Cell Phone Number: {cell}
+          </div>
+          <div style={{ marginBottom: "10px" }} className="profileInfo">
+            Based Out Of: {city}
+          </div>
+          <div style={{ marginBottom: "10px" }} className="profileInfo">
+            Description: {description}
+          </div>
+        </div>
+        <Link to="/updateProfile">
+          <button style={{ backgroundColor: "#eb8c34", marginTop: "10px" }} type="button">
+            Update Profile
+          </button>
+        </Link>
+        <div>
+          {InstaPosts[0] ? (
+            <div>
+              <img src={InstaPosts[0].imageUrl} />
+              <img src={InstaPosts[1].imageUrl} />
+              <img src={InstaPosts[2].imageUrl} />
+              <img src={InstaPosts[3].imageUrl} />
+              <img src={InstaPosts[4].imageUrl} />
+              <img src={InstaPosts[5].imageUrl} />
+            </div>
+          ) : null}
+        </div>
+      </Grid>
     </div>
   );
 };

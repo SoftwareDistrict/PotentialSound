bedc85a1f48e915caedf50f8bf36949be15923a6
HenokSamuel <henoksamuel360@gmail.com>
2020-09-16 23:30:11 -0500

    (refactor)-Moved makeStyles obj to  styles page

--- client/src/components/ChatEntry.jsx
+++ client/src/components/ChatEntry.jsx
@@ -3,40 +3,13 @@ import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import { AvatarGroup } from "@material-ui/lab";
 import { Avatar } from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
-import { dark, light } from "../styles/styles.js";
+import { dark } from "../styles/styles.js";
+import { chatEntryStyles } from "../styles/styles.js";
 
 const ChatEntry = ({ participants, id_chat }) => {
   const members = [];
 
-  const useStyles = makeStyles({
-    avatar: {
-      height: 50,
-      width: 50,
-      variant: "circle",
-    },
-    avatarGroup: {
-      display: "flex",
-      alignItems: "center",
-      justifyContent: "center",
-    },
-    text: {
-      color: "#white",
-    },
-    pDiv: {
-      background: light,
-      boxShadow: "0px 0px 0px 4px #8D99AE",
-      border: "5px solid",
-      borderColor: "white",
-      margin: "0 auto",
-      borderTopLeftRadius: "5px",
-      bordeBottomRightRadius: "5px",
-      position: "relative",
-      alignItems: "center",
-      height: "100px",
-    },
-  });
-  const classes = useStyles();
+  const classes = chatEntryStyles();
 
   participants.forEach((member) => {
     if (member.chatId === id_chat) {

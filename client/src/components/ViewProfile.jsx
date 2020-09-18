bc9af2125b003336d54ff9fb655ed2842bf3d046
BrandonSB <bsbrionesos.gmail.com>
2020-09-17 21:01:05 -0500

    (refac) images and icon fixed

--- client/src/components/ViewProfile.jsx
+++ client/src/components/ViewProfile.jsx
@@ -51,41 +51,33 @@ const ViewProfile = ({ match, currentUser }) => {
         >
           <Grid container direction="row" justify="center" alignItems="center">
             <Container className={classes.container}>
-              <Typography variant="h3" align="center">
+              <Typography variant="h3" align="center" className={classes.username}>
                 {username}
               </Typography>
               <Avatar alt={username} src={proInfo.propic} className={classes.avatar} />
               <div>
                 <Grid container justify="center" alignItems="flex-start" direction="column">
-                  <Grid container justify="space-evenly" alignItems="center" direction="row">
-                    <div style={{ fontSize: "24px", paddingLeft: 20, marginTop: "20px" }}>
+                  <Grid container justify="center" alignItems="center" direction="row">
+                    <div style={{ fontSize: 35, marginTop: "20px" }}>
                       {proInfo.youTube ? (
-
                         <a href={proInfo.youTube} style={{ marginRight: "20px" }}>
-                          <FontAwesomeIcon color="red" icon={faYoutube} size="lg" />
+                          <FontAwesomeIcon color="red" icon={faYoutube} size="sm" />
                         </a>
-
                       ) : null}
                       {proInfo.instagram ? (
-
                         <a href={proInfo.instagram} style={{ marginRight: "20px" }}>
-                          <FontAwesomeIcon color="#e4405f" icon={faInstagram} size="lg" />
+                          <FontAwesomeIcon color="#e4405f" icon={faInstagram} size="sm" />
                         </a>
-
                       ) : null}
                       {proInfo.soundCloud ? (
-
                         <a href={proInfo.soundCloud} style={{ marginRight: "20px" }}>
-                          <FontAwesomeIcon color="red" icon={faSoundcloud} size="lg" />
+                          <FontAwesomeIcon color="red" icon={faSoundcloud} size="sm" />
                         </a>
-
                       ) : null}
                       {proInfo.facebook ? (
-
                         <a href={proInfo.facebook} style={{ marginRight: "20px" }}>
-                          <FontAwesomeIcon color="blue" icon={faFacebook} size="lg" />
+                          <FontAwesomeIcon color="blue" icon={faFacebook} size="sm" />
                         </a>
-
                       ) : null}
                     </div>
                   </Grid>
@@ -102,25 +94,25 @@ const ViewProfile = ({ match, currentUser }) => {
             </Container>
             <div>
               {instaPic[0] ? (
-                <div className={classes.root}>
+                <div>
                   <GridList cols={2}>
-                    <GridListTile className={classes.img}>
-                      <img src={instaPic[0].imageUrl} />
+                    <GridListTile>
+                      <img src={instaPic[0].imageUrl} className={classes.gridList} />
                     </GridListTile>
-                    <GridListTile className={classes.img}>
-                      <img src={instaPic[1].imageUrl} />
+                    <GridListTile>
+                      <img src={instaPic[1].imageUrl} className={classes.gridList} />
                     </GridListTile>
-                    <GridListTile className={classes.img}>
-                      <img src={instaPic[2].imageUrl} />
+                    <GridListTile>
+                      <img src={instaPic[2].imageUrl} className={classes.gridList} />
                     </GridListTile>
-                    <GridListTile className={classes.img}>
-                      <img src={instaPic[3].imageUrl} />
+                    <GridListTile>
+                      <img src={instaPic[3].imageUrl} className={classes.gridList} />
                     </GridListTile>
-                    <GridListTile className={classes.img}>
-                      <img src={instaPic[4].imageUrl} />
+                    <GridListTile>
+                      <img src={instaPic[4].imageUrl} className={classes.gridList} />
                     </GridListTile>
-                    <GridListTile className={classes.img}>
-                      <img src={instaPic[5].imageUrl} />
+                    <GridListTile>
+                      <img src={instaPic[5].imageUrl} className={classes.gridList} />
                     </GridListTile>
                   </GridList>
                 </div>
